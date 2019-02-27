e6c0ae300a96bef846f1d973877a322c19203fe8
lavedon <lavedon@gmail.com>
2019-02-27 02:05:33 -0500

    added both 1 second polling and tabs.onUpdated listener.

--- video_speed_up-master/video_speed_up-master/background.js
+++ video_speed_up-master/video_speed_up-master/background.js
@@ -1,4 +1,15 @@
+try {
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log('Page uses History API and we heard a pushState/replaceState');
     chrome.tabs.executeScript(null, {file: "content.js"});		
+}); 
+} catch(err) {
+    console.log(err);
+}
+try {
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.executeScript(null, {file: "content.js"});
 });
+} catch(err) {
+    console.log(err);
+}
--- video_speed_up-master/video_speed_up-master/content.js
+++ video_speed_up-master/video_speed_up-master/content.js
@@ -1,18 +1,15 @@
-console.log("Speed up all videos...go?");
-
 try {
+    function speed_up_videos() {
+    console.log("Called speed_up_videos()");
     var videos = window.document.querySelectorAll('video');
     console.log("There are " + videos.length + " videos found");
-    function speed_up_videos() {
         for (i=0;i < videos.length;i++) {
             console.log("video is " + videos[i]);
-            videos[i].playbackRate = 2.75;
+            videos[i].playbackRate = 3.1;
         }
     }
     speed_up_videos();
-    setTimeout(speed_up_videos(), 5000);
-    setTimeout(speed_up_videos(), 15000);
-    console.log("confirm sped up");
+    setInterval(speed_up_videos(), 1000);
 }
 catch(err) {
     console.log(err + " couldn't speed up video!");
